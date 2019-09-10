de3487c834fba58aa68b91f2549806bf4bd2b3ea
李捷 <jackyli706@gmail.com>
2019-09-10 14:38:06 +0800

    新增3个接口

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import json
+import time
+
+from flask import Flask, request
 from flask_apscheduler import APScheduler
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -12,17 +13,16 @@ from spider import Spider
 app = Flask(__name__)
 # 读取配置文件
 app.config.from_object('config')
-print(type(app.config['DB_PORT']))
 # 初始化数据库
 engine = create_engine(
-    "mysql+pymysql://" + app.config['DB_USERNAME'] + ":" + app.config['DB_PASS'] + "@" + app.config['DB_IP'] + ":" +
-    app.config['DB_PORT'] + "/news", max_overflow=5)
+    app.config['SQLALCHEMY_DATABASE_URI'], max_overflow=5)
+conn = engine.connect()
 Base = declarative_base()
 db = SQLAlchemy(app)
 
 
-# 创建表
-class Article(Base):
+# 创建新闻内容表
+class NewsContent(Base):
     __tablename__ = "content"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     url = db.Column(db.String(100), nullable=False)  # 文章详细内容链接
@@ -34,6 +34,14 @@ class Article(Base):
     pic = db.Column(db.Text, nullable=False)  # 图片地址
 
 
+# 创建用户表
+class User(Base):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(100), nullable=False)  # 用户名
+    password = db.Column(db.String(100), nullable=False)  # 密码
+
+
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -43,21 +51,83 @@ session = Session()
 def add_data():
     splder = Spider()
     new_list = splder.go()
-    ret = session.query(Article).order_by(Article.id.desc()).first()  # 查询时间最大的那条数据
+    ret = session.query(NewsContent).order_by(NewsContent.id.desc()).first()  # 查询时间最大的那条数据
     for new in new_list:
         if ret is not None and int(ret.time) >= new.time:
             continue
         else:
-            article = Article(url=new.url, title=new.title, content=new.content, time=new.time,
+            article = NewsContent(url=new.url, title=new.title, content=new.content, time=new.time,
                               origin=new.origin, pic=new.pic, formattime=new.formattime)
             session.add(article)
             session.commit()
 
 
-@app.route('/')
+@app.route('/query', methods=['GET', 'POST'])
 def query():
-    ret = session.query(Article).order_by(Article.id.desc()).first()
-    return str(ret.time)
+    lasttime = 0 if request.args.get('lasttime') is None else request.args.get('lasttime')
+    retmax = session.query(NewsContent).order_by(NewsContent.id.desc()).first()  # 查询时间最大的那条数据
+    ret = session.query(NewsContent).order_by(NewsContent.id.desc()).limit(10)
+    list = []
+    jsonreturn = {}
+    jsonreturn["lasttime"] = retmax.time
+    for i in range(10):
+        json = {}
+        if int(lasttime) > int(ret[i + 1].time):
+            continue
+        json["time"] = ret[i + 1].time
+        json["url"] = ret[i + 1].url
+        json["title"] = ret[i + 1].title
+        json["content"] = ret[i + 1].content
+        json["time"] = ret[i + 1].time
+        json["formattime"] = ret[i + 1].formattime
+        json["origin"] = ret[i + 1].origin
+        json["pic"] = ret[i + 1].pic
+        list.append(json)
+
+    jsonreturn["data"] = list
+
+    # if lasttime == 0:
+    #
+    # else:
+    #     ret = session.query(NewsContent).order_by(NewsContent.id.desc()).limit(10)
+    return jsonreturn
+
+
+# 登录
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        username = request.args.get('username')
+    else:
+        username = request.json['username']
+    print(request.json)
+    user = session.query(User).filter_by(username=username).first()
+    if user is None:
+        return "用户名不存在,请注册"
+    else:
+        password = request.args.get('password')
+        if password == user.password:
+            return "登录成功"
+        else:
+            return "密码错误"
+
+
+# 注册
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        username = request.args.get('username')
+    else:
+        username = request.json['username']
+    user = session.query(User).filter_by(username=username).first()
+    if user is not None:
+        return "用户名存在,请登录"
+    else:
+        password = request.args.get('password')
+        user = User(username=username, password=password)
+        session.add(user)
+        session.commit()
+        return "注册成功"
 
 
 if __name__ == '__main__':
@@ -66,6 +136,5 @@ if __name__ == '__main__':
     scheduler.init_app(app)
     scheduler.add_job(func=add_data, id='1', args=(), trigger='interval', seconds=1 * 60 * 60, replace_existing=True)
     scheduler.start()
-
     # 运行
     app.run(debug=app.config['DEBUG'], port=app.config['PORT'], host=app.config['IP'])
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # 服务器配置文件
 # 服务器配置
 DEBUG = True
-PORT = 9080
+PORT = 9082
 IP = '127.0.0.1'
 
 # 数据库配置
@@ -9,3 +9,10 @@ DB_USERNAME = 'root'
 DB_PASS = '123456'
 DB_PORT = '3306'
 DB_IP = "127.0.0.1"
+DB_NAME = 'news'
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{db}?charset=utf8mb4".format(
+    username=DB_USERNAME,
+    password=DB_PASS,
+    host=DB_IP, port=DB_PORT,
+    db=DB_NAME)
+SQLALCHEMY_TRACK_MODIFICATIONS = True
